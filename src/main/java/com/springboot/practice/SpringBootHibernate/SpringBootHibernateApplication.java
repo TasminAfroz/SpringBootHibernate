30a1743c6b36896940fd8dc9c381af14b1146ed7
Tasmin Afroz <tasmin.afroz@bjitgroup.com>
2018-02-07 10:49:13 +0600

    only the raw project added

---
+++ src/main/java/com/springboot/practice/SpringBootHibernate/SpringBootHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.practice.SpringBootHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringBootHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateApplication.class, args);
+	}
+}
